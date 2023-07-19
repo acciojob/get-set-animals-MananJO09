aa4f3f1ebc76b24ecff2d70ecc401e3f080bd833
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-07-20 00:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log("Animal sound");
+  }
+}
+
+// Subclass: Cat
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Subclass: Dog
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
